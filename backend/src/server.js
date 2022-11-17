0370dafd352bd3ffdaeb903b5fca6ef39391034c
Qing Feng <fengqingthu@gmail.com>
2022-11-17 01:56:36 -0500

    Implement HTTP handler

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 const http = require('http');
-
-
-import {Query, GetPage} from './fetcher';
+const fetcher = require('./fetcher');
 
 // =========================== Global Constants ===========================
-
-
 
 const HOST = 'localhost';    // host that run the backend server
 const SERVER_PORT = 8000;    // the port to receive http request
 
+const CACHE_DIR = fetcher.CACHE_DIR;
+
 const _init_directory = (dir) => {
-    // if dir exists, reset.
+    // if dir already exists, reset.
     if (fs.existsSync(dir))
         fs.rmSync(dir, { recursive: true, force: true });
     fs.mkdirSync(dir);
@@ -21,60 +19,79 @@ const _init_directory = (dir) => {
 
 // =========================== End Constants ===========================
 
-const _process_request = async (req) => {
-    // parse request
-
-    // get the sql query from body
-    let body = ''
-    await req.on('data', (data) => {
-        body += data
-    })
-    await req.on('end', () => {
-    })
-
-    let fields = req.url.split('/').slice(1,)
-
-    let query_text = body
-    let read = (fields[0] === 'read')
-
-    console.log(`Get HTTP request: Query: ${query_text}, Read-only: ${read}`)
-
-    
-}
-
 // =========================== HTTP Handler ===========================
+
+/**
+ * Usage:
+ * curl -X POST http://localhost:8000/api/read \
+ * -H 'Content-Type: application/json' \
+ * -d '{"query": SELECT * FROM ...}'
+ * Response: {"num_page": 5}
+ * 
+ * curl -X POST http://localhost:8000/api/update \
+ * -H 'Content-Type: application/json' \
+ * -d '{"query": UPDATE ...}'
+ * Response: {"num_page": -1}
+ * 
+ * curl -X POST http://localhost:8000/api/get-page \
+ * -H 'Content-Type: application/json' \
+ * -d '{"page_index": 0}'
+ * Response: {"page_data": [JSON_DATA]} */
+
 const requestListener = async (req, res) => {
-    res.setHeader("Content-Type", "application/json");
     var segs = req.url.split('/').slice(1,);
-    console.log(`Receive request:${segs}`);
+    if (segs[0] != "api") {
+        res.writeHead(400);
+        res.end();
+        return;
+    }
 
+    // Parse JSON body
+    var arr = [];
+    await req.on("data", function (data) {
+        arr.push(data);
+    });
+    var data = Buffer.concat(arr).toString(), ret;
     try {
-        res.writeHead(200);
-        switch(segs[0]) {
-            case "read":
-
-                let num_block = await Query()
-        }
-
-        let num_block = await _process_request(req)
-
-        res.writeHead(200)
-        res.end(num_block.toString())
+        var ret = JSON.parse(data);
     } catch (err) {
-        console.log("requestListener::Unexpected!")
+        res.writeHead(400);
+        res.end();
+        return;
+    }
 
-        res.writeHead(400)
-        res.end()
+    // Handle requests
+    try {
+        res.writeHead(200, { "Content-Type": "application/json" });
+        switch (segs[1]) {
+            case "read":
+                let num_page = await fetcher.Query(ret.query, true);
+                res.end(JSON.stringify({ "num_page": num_page }));
+
+            case "update":
+                await fetcher.Query(ret.query, false);
+                res.end(JSON.stringify({ "num_page": -1 }));
+
+            case "get-page":
+                const page = await fetcher.GetPage(ret.query, ret.page_index)
+                res.end(JSON.stringify({ "page_data": page }));
+            default:
+                res.writeHead(404);
+                res.end();
+        }
+    } catch (err) {
+        res.writeHead(500);
+        res.end();
     }
 }
 
 const Launch = async () => {
-    console.log(`Initializing cache folder: ${CACHE_DIR}`)
-    _init_directory(CACHE_DIR)
+    console.log(`Initializing cache folder: ${CACHE_DIR}`);
+    _init_directory(CACHE_DIR);
 
-    console.log("========== Listening to HTTP request ==========")
+    console.log("========== Listening to HTTP request ==========");
     const server = http.createServer(requestListener);
     server.listen(SERVER_PORT, HOST, () => {
-        console.log(`Server is running on http://${HOST}:${SERVER_PORT}`)
-    })
+        console.log(`Server is running on http://${HOST}:${SERVER_PORT}`);
+    });
 }; Launch();
